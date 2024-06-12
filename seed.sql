012a4f50ce1fc0ce211f7d1f6a7bf6b4c56e66a6
DAVINA SINGH <davina_singh@outlook.com>
2024-06-12 14:23:16 +0100

    it works

--- seed.sql
+++ seed.sql
@@ -1,3 +1,8 @@
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY,
+    category VARCHAR
+);
+
 CREATE TABLE posts(
     post_id SERIAL PRIMARY KEY,
     title VARCHAR,
@@ -5,13 +10,11 @@ CREATE TABLE posts(
     category_id INTEGER REFERENCES categories(category_id)
 );
 
-CREATE TABLE categories (
-    category_id SERIAL PRIMARY KEY,
-    category VARCHAR
-);
-
 CREATE TABLE comments (
    comment_id SERIAL PRIMARY KEY,
    comment TEXT,
    post_id INTEGER REFERENCES posts(post_id)
 );
+
+INSERT INTO categories (category)
+VALUES ('Travel'), ('Food'), ('Movies');
